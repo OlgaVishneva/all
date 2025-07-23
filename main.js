cbae077a660fd5ce56954ca95a69f14f25e2bfd7
Olga <olga1111vishneva.v@mail.ru>
2025-07-23 14:12:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import '../scss/main.scss';
+import dropdown from './scripts/dropdown.js';
+import openMenu from './scripts/openMenu.js';
+import progress from './scripts/progress.js';
+import scroll from './scripts/scroll.js';
+
+dropdown();
+openMenu();
+progress();
+scroll()
